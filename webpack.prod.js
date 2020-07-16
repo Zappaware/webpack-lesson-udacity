bd1510453376fa88139929a0d9aa6a30b15f0516
Sergio Cervantes <50383374+Zappaware@users.noreply.github.com>
2020-07-15 22:49:51 -0500

    This commit just have a few tweaks in the config files and the server side code to work with webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,11 +4,15 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: './src/client/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'webpack.bundle.js'
+    },
     mode: 'production',
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
             }
