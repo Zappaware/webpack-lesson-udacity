22a3ecb8b57488715c9c368410e5bf7869d46797
Sergio Cervantes <50383374+Zappaware@users.noreply.github.com>
2020-07-18 22:37:14 -0500

    This commit has a lot of changes, configurations and more to do all sort of thins, like minifying css, making a fetch request to a API, etc

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,14 +3,33 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    fetch('http://localhost:8081/test')
+    .then(res => {
+     return res.json()
+ })
+    .then(function(data) {
+        document.getElementById('results').innerHTML = data.message
+ })
+ 
+let zipCode = document.getElementById('zip').value
+let countryCode = document.getElementById('country').value
+let appId = `23eaaf61fab62a2f43f0fc31f3893e2a`
+const baseUrl = `api.openweathermap.org/data/2.5/weather?`
+let url = `http://${baseUrl}zip=${zipCode},${countryCode}&appid=${appId}&units=imperial`
+console.log("::: Weather Form Submitted :::")
+    fetch(url)
+    .then(res => {
+     return res.json()
+ })
+    .then(function(data) {
+        alert(data.main.temp)
+ })
 }
+
+
+
 
 export { handleSubmit }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,20 +1,33 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
     output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: 'webpack.bundle.js'
+      filename: 'webpack.bundle.js',
+      libraryTarget: 'var',
+      library: 'Client'
     },
     mode: 'production',
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+        },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             }
         ]
     },
@@ -22,6 +35,8 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({ filename: "main.css" }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
