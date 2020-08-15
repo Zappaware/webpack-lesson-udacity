08d92553d33180c30228bb2f84e29f3d2726bf5e
Sergio Cervantes <50383374+Zappaware@users.noreply.github.com>
2020-08-14 19:34:11 -0500

    This a functional webpage with one POST request and running on webpack. It needs to more to be done

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -14,19 +14,65 @@ function handleSubmit(event) {
         document.getElementById('results').innerHTML = data.message
  })
  
-let zipCode = document.getElementById('zip').value
-let countryCode = document.getElementById('country').value
+
+console.log("::: MeaningCloud Form Submitted :::")
+   const meaningCloudResponse= async (url) =>{
+       const response = await fetch (url,{
+           method: 'POST',
+           credentials: 'same-origin',
+           mode: "cors"       
+       })
+
+       try {
+           const newData= response.json();
+           return newData
+
+       } catch (error){
+           console.log('error', error)
+
+       }
+   }
+   const userResponse= async (url, data = {}) =>{
+    const response = await fetch (url,{
+        method: 'POST',
+        credentials: 'same-origin',
+        mode: "cors"       
+    })
+
+    try {
+        const newData= response.json();
+        return newData
+
+    } catch (error){
+        console.log('error', error)
+
+    }
+}
+
+   const getData = async (url='') => {
+    const request = await fetch(url);
+
+    try{
+        const allData = await request.json();
+        console.log(allData);
+    } catch (error) {
+        console.log('error', error)
+    }
+}
+
+let feeling = document.getElementById('feeling').value
 let appId = ``
-const baseUrl = `api.openweathermap.org/data/2.5/weather?`
-let url = `http://${baseUrl}zip=${zipCode},${countryCode}&appid=${appId}&units=imperial`
-console.log("::: Weather Form Submitted :::")
-    fetch(url)
-    .then(res => {
-     return res.json()
- })
-    .then(function(data) {
-        alert(data.main.temp)
- })
+const baseUrl = `https://api.meaningcloud.com/sentiment-2.1?`
+let url = `${baseUrl}key=${appId}&of=json&txt=${feeling}&model=sentiment&lang=en`
+
+meaningCloudResponse(url)
+.then((data)=>{
+    userResponse('/add', {
+      projectData: data  
+    })
+.then(getData('/all'))   
+})
+   
 }
 
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,19 @@
+projectData = {}
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  })
 
 app.use(express.static('dist'))
 
@@ -20,6 +31,16 @@ app.listen(8081, function () {
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
+})
+
+app.post('/add', (req, res)=>{
+    projectData = req.body
+    console.log(projectData)
+    res.send(true);
+});
+
+app.get('/all', (req, res)=>{
+    res.send(projectData)
 })
 
 
