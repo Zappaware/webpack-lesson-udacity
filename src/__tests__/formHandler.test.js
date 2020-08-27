807af38bb38d898c17b1a52e36c2541cc74eda48
Sergio Cervantes <50383374+Zappaware@users.noreply.github.com>
2020-08-27 13:48:44 -0500

    This commit has the jest testing functionallity and have some unit testing, but I would like to have more functions to be tested, the service workers are already working, so I need to comment out the project and It will be done

---
+++ src/__tests__/formHandler.test.js
@@ -0,0 +1,8 @@
+const handleSubmit = require('../client/js/formHandler')
+  
+describe("Testing the submit functionality", () => {
+      
+    test("Testing the handleSubmit() function, it should return PASS", () => {
+           expect(handleSubmit).toBeDefined();
+})});
+
---
+++ src/__tests__/nameChecker.test.js
@@ -0,0 +1,7 @@
+const checkForName = require('../client/js/nameChecker')
+   
+describe("Testing the nameChecker functionality", () => {
+ 
+    test("Testing the checkForName() function, it should return PASS", () => {
+           expect(checkForName).toBeDefined();     
+})});
